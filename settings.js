d88c0da3a0178f7cf0bc9d38b3422b952643d0b4
¡X <104746323+Xdit133@users.noreply.github.com>
2022-05-24 18:16:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,9 +27,9 @@ global.premium = ['6285641476033'] //ubah jadi nomor mu, note tanda ' gausah di 
 global.ownernomer = '6285641476033' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Raditya' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = '® Raditya¡X' //ubah jadi nama mu, note tanda ' gausah di hapus!
 global.botname = 'Xdit-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Raditya' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.footer = '® Raditya¡X' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -42,7 +42,7 @@ global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
 global.packname = 'Di Buat Oleh Xdit-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Raditya-¡X' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.author = 'Insta @hai_om.qw' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
